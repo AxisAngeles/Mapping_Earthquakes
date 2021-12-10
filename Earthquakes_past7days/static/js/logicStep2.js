e510c544e40954e1dff0eb562f8ac93ba87fae91
Alexis Angeles <axis.angeles@gmail.com>
2021-12-10 17:17:43 -0600

    Adding Earthquake data styling.

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,104 @@
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+
+// ******** MapBox Styles API **********
+// Streets view tile layer that will be an option for our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let strsatelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+            // // Light view tile layer that will be an option for our map.
+            // let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            // attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+            //     maxZoom: 18,
+            //     accessToken: API_KEY
+            // });
+
+            // // Dark view tile layer that will be an option for our map.
+            // let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            // attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+            //     maxZoom: 18,
+            //     accessToken: API_KEY
+            // });
+
+// 3) Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// 4) Create a base layer that holds both maps.
+let baseMaps = {
+    Streets: streets,
+    Satellite: strsatelliteStreets
+};
+
+
+// 1) Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// 1) Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+})
+
+// 1.5) Pass our map layers into our LAYERS CONTROL and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// 2) Add GeoJSON data within the web
+let torontoHoods = "https://raw.githubusercontent.com/AxisAngeles/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// 3) Create a style FUNCITON based on features data.
+    // This function returns the style data for each of the earthquakes we plot on
+    // the map. We pass the magnitude of the earthquake into a function
+    // to calculate the radius.
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+}
+
+// 4) getRadius FUNCION.
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+}
+
+
+// 3) Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data,{
+        // We turn each feature into a circleMarker on the map.
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+            },
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
+    }).addTo(map);
+
+});
